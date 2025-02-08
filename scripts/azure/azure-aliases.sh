297823df45179a9b31b9b40e882f55df59b1b65d
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-02-08 16:37:54 +0100

    fix(scripts): extend az cli aliases

--- scripts/azure/azure-aliases.sh
+++ scripts/azure/azure-aliases.sh
@@ -25,9 +25,11 @@ function get_aks_cluster {
   local CLUSTERS=$(az aks list --query "[].{name:name,rg:resourceGroup}" -o json)
   local COUNT=$(echo $CLUSTERS | jq '. | length')
 
-  for i in $(seq 1 $COUNT); do
-    echo "Setting up the kube config for cluster $(echo $CLUSTERS | jq '.['$i-1'].name')"
-    az aks get-credentials --name $(echo $CLUSTERS | jq '.['$i-1'].name' --resource-group $(echo $CLUSTERS | jq '.['$i-1'].rg')
+  for i in $(seq 1 ${COUNT}); do
+    local NAME=$(echo "$CLUSTERS" | jq -r '.['$i-1'].name')
+    local RG=$(echo "$CLUSTERS" | jq -r '.['$i-1'].rg')
+    echo "Setting up the kube config for cluster $NAME"
+    az aks get-credentials --name $NAME --resource-group $RG
   done
 }
 
@@ -35,3 +37,46 @@ alias azkg=get_aks_cluster
 alias azls='az resource list -o table'
 alias azrgl='az group list -o table'
 
+function get_aks_overview {
+  local CLUSTERS=$(az aks list --query "[].{name:name,rg:resourceGroup}" -o json)
+  local COUNT=$(echo $CLUSTERS | jq '. | length')
+
+  for i in $(seq 1 ${COUNT}); do
+    local NAME=$(echo "$CLUSTERS" | jq -r '.['$i-1'].name')
+    local RG=$(echo "$CLUSTERS" | jq -r '.['$i-1'].rg')
+    az aks show -n $NAME -g $RG -o table
+  done
+}
+
+alias azks=get_aks_overview
+
+function update_aks_clusters {
+  local CLUSTERS=$(az aks list --query "[].{name:name,rg:resourceGroup}" -o json)
+  local COUNT=$(echo $CLUSTERS | jq '. | length')
+
+  for i in $(seq 1 ${COUNT}); do
+    local NAME=$(echo "$CLUSTERS" | jq -r '.['$i-1'].name')
+    local RG=$(echo "$CLUSTERS" | jq -r '.['$i-1'].rg')
+    az aks update -n $NAME -g $RG
+  done
+}
+
+alias azkup=update_aks_clusters
+
+function upgrade_kubernetes_version {
+  local CLUSTERS=$(az aks list --query "[].{name:name,rg:resourceGroup}" -o json)
+  local COUNT=$(echo $CLUSTERS | jq '. | length')
+
+  for i in $(seq 1 ${COUNT}); do
+    local NAME=$(echo $CLUSTERS | jq -r '.['$i-1'].name')
+    local RG=$(echo $CLUSTERS | jq -r '.['$i-1'].rg')
+    local VERSIONS=$(az aks get-upgrades -n $NAME -g $RG --query "controlPlaneProfile.upgrades[].kubernetesVersion")
+    echo "Available upgrades: "
+    echo $VERSIONS | jq
+    local HIGHEST=$(echo $VERSIONS | jq 'sort_by(split(".")|map(tonumber))' | jq -r '.[-1]')
+    echo "Upgrading to version: $HIGHEST"
+    az aks upgrade --kubernetes-version $HIGHEST --name $NAME --resource-group $RG --no-wait
+  done
+}
+
+alias azkU=upgrade_kubernetes_version
