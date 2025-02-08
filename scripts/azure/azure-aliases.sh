0e1f4cfac5b499f5483b5d208276d2da0e6d02fc
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-02-08 13:08:02 +0100

    fix(scripts): extend az cli aliases

--- scripts/azure/azure-aliases.sh
+++ scripts/azure/azure-aliases.sh
@@ -14,6 +14,7 @@ function log_to_azure {
     echo "The chosen name either does not uniquely identify a subscription or none at all."
   else
     az account set --subscription $(echo $ACCOUNTS | jq -r '.[].id')
+    az account show
   fi
 }
 
@@ -24,12 +25,13 @@ function get_aks_cluster {
   local CLUSTERS=$(az aks list --query "[].{name:name,rg:resourceGroup}" -o json)
   local COUNT=$(echo $CLUSTERS | jq '. | length')
 
-  for $i in $(seq 1 $COUNT); do
+  for i in $(seq 1 $COUNT); do
     echo "Setting up the kube config for cluster $(echo $CLUSTERS | jq '.['$i-1'].name')"
     az aks get-credentials --name $(echo $CLUSTERS | jq '.['$i-1'].name' --resource-group $(echo $CLUSTERS | jq '.['$i-1'].rg')
   done
 }
 
 alias azkg=get_aks_cluster
-
+alias azls='az resource list -o table'
+alias azrgl='az group list -o table'
 
